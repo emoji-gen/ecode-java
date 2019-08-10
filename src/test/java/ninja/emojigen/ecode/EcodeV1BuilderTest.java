9ca7346884a3fec2d94be688bbae6d35820585aa
Pine Mizune <pinemz@gmail.com>
2019-08-10 18:37:05 +0900

    test: add tests

---
+++ src/test/java/ninja/emojigen/ecode/EcodeV1BuilderTest.java
@@ -0,0 +1,23 @@
+package ninja.emojigen.ecode;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+@SuppressWarnings("ConstantConditions")
+public class EcodeV1BuilderTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void textTest_null() {
+        expectedException.expect(NullPointerException.class);
+        new EcodeV1Builder().text(null);
+    }
+
+    @Test
+    public void textTest_empty() {
+        expectedException.expect(IllegalArgumentException.class);
+        new EcodeV1Builder().text("");
+    }
+}
