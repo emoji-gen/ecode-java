b4e36feb8e2f10fbac3e830a9effe7b3a2bb5551
Pine Mizune <pinemz@gmail.com>
2019-08-10 15:41:57 +0900

    test: add tests

---
+++ src/test/java/ninja/emojigen/ecode/EcodeFlagTest.java
@@ -0,0 +1,13 @@
+package ninja.emojigen.ecode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EcodeFlagTest {
+    @Test
+    public void fromIdTest() {
+        assertEquals(EcodeFlag.SIZE_FIXED, EcodeFlag.fromId(EcodeFlag.SIZE_FIXED.getId()));
+        assertEquals(EcodeFlag.STRETCH, EcodeFlag.fromId(EcodeFlag.STRETCH.getId()));
+    }
+}
